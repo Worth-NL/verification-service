99713e1bc507076a2df19550cbedd4a69a646f68
Frank Molenaar <frankmolenaarma@gmail.com>
2025-08-26 11:10:11 +0200

    adds cron task

--- app/api/swagger/route.ts
+++ app/api/swagger/route.ts
@@ -8,7 +8,7 @@ export async function GET() {
             info: {
                 title: "Verification Requests API",
                 description: "API to create and verify email verification requests",
-                version: "0.1.2",
+                version: "0.1.3",
             },
             servers: [{ url: "/api" }],
             paths: {
