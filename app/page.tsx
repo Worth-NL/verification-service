a63294b87de1652034831e432c22e2519161bde5
frankmolenaar1986 <frankmolenaarma@gmail.com>
2025-09-17 10:21:36 +0200

    Add phone number functionality
    * removes next-swagger-doc
    
    * adds docker config
    
    * adds docker config
    
    * bumps version number
    
    * removes templateid
    
    * Merge branch 'adding-docker' of github.com:Worth-NL/verification-service into adding-docker
    
    * adds phone number functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-"use client";
-import React, { useEffect } from "react";
+"use client"
+import { RedocStandalone } from 'redoc';
 
 export default function RedocViewer() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js";
-    script.async = true;
-    document.body.appendChild(script);
-  }, []);
-
   return (
-    <div style={{ height: "100vh" }}>
-      {React.createElement("redoc", { "spec-url": "/api/swagger" })}
-    </div>
+    <RedocStandalone
+      specUrl="/api/swagger"
+      options={{
+        nativeScrollbars: true
+      }}
+    />
   );
 }
